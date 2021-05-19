3b8e8b1122383bd8638965b2063fe91af51bdec2
Konrad Radoń <konradradon@gmail.com>
2021-05-20 00:22:29 +0200

    Add me endpoint

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 
 import { AuthService } from '../service/auth.service';
 import { LoginDto } from '../../models/login.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard';
 
 @Controller()
 export class AuthController {
   constructor(private enrollService: AuthService) {}
 
   @Post('login')
-  public async login(@Body() user: LoginDto): Promise<{ role: string }> {
+  public async login(@Body() user: LoginDto): Promise<{ username: string; token: string; role: string }> {
     return this.enrollService.login(user.username, user.password);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  public me(@Request() req): { username: string; role: string } {
+    return req.user;
   }
 }
